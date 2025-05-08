20b7ce830dcc7425c2f2ca1ed35b2437af011634
Jacob Rice <jrice909@csu.fullerton.edu>
2025-05-08 08:31:30 -0700

    Created the TaskRepostitory.kt file. Still more to change

---
+++ app/src/main/java/com/example/finaltodo/TaskRepostitory.kt
@@ -0,0 +1,35 @@
+package com.example.finaltodo
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+
+class TaskRepostitory(context: Context) {
+    private val dbHelper = TaskDataHelper(context)
+
+    fun addTask(task: String) {
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(TaskDataHelper.COLUMN_TASK, task)
+        }
+        db.insert(TaskDataHelper.TABLE_NAME, null, values)
+    }
+
+    fun getAllTasks(): List<String> {
+        val db = dbHelper.readableDatabase
+        val cursor: Cursor = db.query(
+            TaskDataHelper.TABLE_NAME,
+            arrayOf(TaskDataHelper.COLUMN_TASK),
+            null, null, null, null, null
+        )
+        val tasks = mutableListof<String>()
+        with(cursor) {
+            while (moveToNext()) {
+                val tasks = getString(getColumnIndexOrThrow(TaskDataHelper.COLUMN_TASK))
+                tasks.add(task)
+            }
+        }
+        cursor.close()
+        return tasks
+    }
+}
